83da147d6578ad4af48540a8cc0b37988f09c1c6
Aniket kandrikar <75112852+aniket24122012@users.noreply.github.com>
2022-11-23 14:39:50 +0530

    image - border

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <p id='r1'>Aniket Kandrikar......</p>
-      <p id='r2'>With border......</p>
-      <p id='r3'>with background image....</p>
-      <p id='r4'>With border......</p>
-      <p id='r5'>With border......</p>
+      <center>
+      <p id="r1">
+        Aniket Kandrikar..... i try
+        djghjdgjgdjfgufgsdjfsgfhjs
+        bsfhjsgfhjsgjhsgfhsghfjsfh
+      </p>
+      </center>
     </div>
   );
 }
